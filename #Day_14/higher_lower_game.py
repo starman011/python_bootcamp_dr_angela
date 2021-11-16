e04926c72610697eb10ea3646e04c8e82aaebef8
starman011 <saqlainkhanofficialo1@gmail.com>
2021-11-17 00:34:59 +0530

    Day 15

--- #Day_14/higher_lower_game.py
+++ #Day_14/higher_lower_game.py
@@ -1,8 +1,7 @@
 from game_data import data
 import random
 from art import logo, vs
-
-
+#TODO: 1. this is the first
 def get_random_account():
   """Get data from random account"""
   return random.choice(data)
@@ -48,8 +47,8 @@ def game():
     b_follower_count = account_b["follower_count"]
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-    import os
-    os.system('cls')
+    from os import system
+    system('cls')
     print(logo)
     if is_correct:
       score += 1
@@ -67,3 +66,27 @@ FAQ: Why does choice B always become choice A in every round, even when A had mo
 Suppose you just started the game and you are comparing the followers of A - Instagram (364k) to B - Selena Gomez (174k). Instagram has more followers, so choice A is correct. However, the subsequent comparison should be between Selena Gomez (the new A) and someone else. The reason is that everything in our list has fewer followers than Instagram. If we were to keep Instagram as part of the comparison (as choice A) then Instagram would stay there for the rest of the game. This would be quite boring. By swapping choice B for A each round, we avoid a situation where the number of followers of choice A keeps going up over the course of the game. Hope that makes sense :-)
 
 '''
+
+
+
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
---
+++ Hacker_rank/print_function.py
@@ -0,0 +1,7 @@
+n = int(input("Whats the number"))
+store = ""
+for n in range(1 , 4):
+    n = n
+    store += n
+
+print(store)
